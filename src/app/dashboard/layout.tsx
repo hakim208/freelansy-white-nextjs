e7012f2692296c88d27f35dac6a6fbc8c2782631
Abdulhakim <inoyatovhakim5@gmail.com>
2025-07-26 15:15:21 +0500

    edit DashboardLayout layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,8 +1,13 @@
+"use client"
+
+import { cn } from "@/lib/utils";
+
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+    const condition = true;
     return (
         <div>
             <nav>
-                <h1 className="mt-[30px] text-4xl font-extrabold text-purple-600  pb-3 inline-block">
+                <h1 className={cn("p-4 text-white", condition && "bg-red-500", !condition && "bg-green-500")}>
                     Список задач
                 </h1>
             </nav>
